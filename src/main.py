b88de93508904a8439170868d2e961ead341a762
GuillaumeHaben <pyxouze@gmail.com>
2017-01-12 15:35:44 +0100

    Preparing input X for DecisionTreeClassifer

--- src/main.py
+++ src/main.py
@@ -3,5 +3,12 @@ from extract import extractCsv
 samples = extractCsv("data.csv")
 print(str(len(samples)) + " connection records extracted")
 print("First sample:")
-for i in samples[0].items():
-    print(i)
+X = []
+for j in samples:
+	line = []
+	for i in j.items():
+		if i[0] != "attack_type":
+			line.append(i[1])
+	X.append(line)
+
+print X[1]
