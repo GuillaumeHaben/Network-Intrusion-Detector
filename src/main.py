859210c1b2e9059cad3a47c15236e04a80796bc9
Julien Déoux <julien.deoux@telecomnancy.net>
2017-01-18 22:40:01 +0100

    [+] Tests with multiple minimum impurity values [-] "data.csv" file, replaced by a "data" folder that is not in the repo

--- src/main.py
+++ src/main.py
@@ -4,56 +4,57 @@ import pydotplus
 
 ''' --- Sample extraction from the training dataset --- '''
 
-print("Extracting connection records from data.csv")
-samples = extractCsv("data.csv")
-print(str(len(samples)) + " connection records extracted")
+print("Extracting connection records from training data")
+trainingSamples = extractCsv("../data/kddcup.data_10_percent_corrected")
+print(str(len(trainingSamples)) + " connection records extracted")
+#print("Extracting connection records from target data")
+#targetSamples = extractCsv("../data/kddcup.data.corrected")
+#print(str(len(targetSamples)) + " connection records extracted")
 
 X = [] # Contains the training samples
 Y = [] # Contains their classes
 
-protocole=[] #contain all the protocole type
-service=[]   #contain all the services
-flag=[]		#contain all the flags
+protocols = [] # Contains the different protocols
+services = [] # Contain the different services
+flags = [] # Contains the different flags
 
-''' TODO
-
-Index of the following parameters:
-  - protocol_type
-  - service
-  - flag
-
-Could be done dynamically while building X
-'''
-
-for j in samples:
+for j in trainingSamples:
     line = []
     for i in j.items():
         if i[0] != "attack_type":
         	if i[0]=="protocol_type":
-        		if i[1] not in protocole:
-        			protocole.append(i[1])
-        		line.append(protocole.index(i[1]))
+        		if i[1] not in protocols:
+        			protocols.append(i[1])
+        		line.append(protocols.index(i[1]))
         	elif i[0]=="service":
-        		if i[1] not in service:
-        			service.append(i[1])
-        		line.append(service.index(i[1]))
+        		if i[1] not in services:
+        			services.append(i[1])
+        		line.append(services.index(i[1]))
         	elif i[0]=="flag":
-        		if i[1] not in flag:
-        			flag.append(i[1])
-        		line.append(flag.index(i[1]))
+        		if i[1] not in flags:
+        			flags.append(i[1])
+        		line.append(flags.index(i[1]))
         	else :
         		line.append(i[1])
         else:
             Y.append(attackTypes.index(i[1]))
     X.append(line)
 
-''' --- Training of the decision tree --- '''
+''' --- Main test loop --- '''
 
-classifier = tree.DecisionTreeClassifier()
-classifier = classifier.fit(X, Y)
+for i in range(1,10):
 
-''' --- Visualisation --- '''
+    ''' --- Training of the decision tree --- '''
 
-dotData = tree.export_graphviz(classifier, out_file=None, class_names=attackTypes, feature_names=fields)
-graph = pydotplus.graph_from_dot_data(dotData)
-graph.write_pdf("attacks.pdf")
+    minImpurity = i/50
+    print("Minimal impurity setting: " + str(minImpurity))
+    classifier = tree.DecisionTreeClassifier(min_impurity_split = minImpurity)
+    classifier = classifier.fit(X, Y)
+
+    ''' --- Visualisation --- '''
+
+    dotData = tree.export_graphviz(classifier, out_file=None, class_names=attackTypes, feature_names=fields)
+    graph = pydotplus.graph_from_dot_data(dotData)
+    graph.write_pdf("attacks_" + str(minImpurity) + ".pdf")
+    print("PDF file generated")
+
