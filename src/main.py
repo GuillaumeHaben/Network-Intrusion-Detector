73fb508bde04123a168397611e0540f3cba2bc50
Julien Déoux <julien.deoux@telecomnancy.net>
2017-01-18 23:19:58 +0100

    [+] Tests on predictions For now, the training samples and the target samples are the same, hence the error rate increases when the minimal impurity does.

--- src/main.py
+++ src/main.py
@@ -7,9 +7,9 @@ import pydotplus
 print("Extracting connection records from training data")
 trainingSamples = extractCsv("../data/kddcup.data_10_percent_corrected")
 print(str(len(trainingSamples)) + " connection records extracted")
-#print("Extracting connection records from target data")
-#targetSamples = extractCsv("../data/kddcup.data.corrected")
-#print(str(len(targetSamples)) + " connection records extracted")
+print("Extracting connection records from target data")
+targetSamples = extractCsv("../data/kddcup.data_10_percent_corrected")
+print(str(len(targetSamples)) + " connection records extracted")
 
 X = [] # Contains the training samples
 Y = [] # Contains their classes
@@ -18,6 +18,7 @@ protocols = [] # Contains the different protocols
 services = [] # Contain the different services
 flags = [] # Contains the different flags
 
+print("Formatting training samples for the decision tree")
 for j in trainingSamples:
     line = []
     for i in j.items():
@@ -39,10 +40,32 @@ for j in trainingSamples:
         else:
             Y.append(attackTypes.index(i[1]))
     X.append(line)
+print("Done")
+
+data = [] # Contains the target samples
+answers = [] # Contains the class to predict
+
+print("Formatting target samples for the decision tree")
+for j in targetSamples:
+    line = []
+    for i in j.items():
+        if i[0] != "attack_type":
+        	if i[0]=="protocol_type":
+        		line.append(protocols.index(i[1]))
+        	elif i[0]=="service":
+        		line.append(services.index(i[1]))
+        	elif i[0]=="flag":
+        		line.append(flags.index(i[1]))
+        	else :
+        		line.append(i[1])
+        else:
+            answers.append(attackTypes.index(i[1]))
+    data.append(line)
+print("Done")
 
 ''' --- Main test loop --- '''
 
-for i in range(1,10):
+for i in range(1,6):
 
     ''' --- Training of the decision tree --- '''
 
@@ -58,3 +81,15 @@ for i in range(1,10):
     graph.write_pdf("attacks_" + str(minImpurity) + ".pdf")
     print("PDF file generated")
 
+    ''' --- Prediction --- '''
+
+    predictions = classifier.predict(data)
+    good = 0
+    bad = 0
+    for i in range(len(predictions)):
+        if predictions[i] == answers[i]:
+            good += 1
+        else:
+            bad +=1
+    errorRate = bad / (good + bad)
+    print("Error rate: " + str(errorRate))
